ace278b13d3fd5501af35fc6ca7d41320a121533
JangHun-00 <janghun0056@gmail.com>
2019-08-10 16:59:05 +0900

    Bubble Sort tmp

--- algorithm_js/BubbleSort copy.js
+++ algorithm_js/BubbleSort copy.js
@@ -1,6 +1,14 @@
-function bubbleSort(data_list){
+String.prototype.format = function () {
+    var i = 0, args = arguments;
+    return this.replace(/{}/g, function () {
+      return typeof args[i] != 'undefined' ? args[i++] : '';
+    });
+  };  
+
+function* bubbleSort(data_list){
     for(var check = (data_list.length - 1); check > 0; check--){
         var xth = data_list.length - check;
+        yield "<br><br>{}번째 정렬<br>".format(xth);
         for(var i = 0; i < check; i++){
             if(data_list[i] > data_list[i+1]){
                 var temp = data_list[i];
